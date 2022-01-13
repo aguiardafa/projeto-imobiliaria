bf57d15647f77fc0a01180cd8194f6e8560563b5
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-13 08:48:42 -0300

    feat(plataforma): implementa na view home o consumo dos imoveis cadastrados

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from plataforma.models import Imovel
 
 
 # Create your views here.
 @login_required(login_url='/auth/logar/')
 def home(request):
-    return render(request, 'home.html')
+    # consulta no Banco de Dados todos os Imóveis cadastrados, através do Model Imovel em Models
+    imoveis = Imovel.objects.all()
+    return render(request, 'home.html', {'imoveis': imoveis})
