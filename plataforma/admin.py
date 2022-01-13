afdf99f191c473488f764394e4a79ace626ae32a
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-13 08:41:51 -0300

    feat(plataforma): registra as novas tabelas para serem acessadas no Admin

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import DiaVisita, Horario, Imovel, Cidade, Imagem, Visita
+
+
 # Register your models here.
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
+    list_editable = ('valor', 'tipo')
+    list_filter = ('cidade', 'tipo')
+
+
+admin.site.register(DiaVisita)
+admin.site.register(Horario)
+admin.site.register(Imagem)
+admin.site.register(Cidade)
+admin.site.register(Visita)
