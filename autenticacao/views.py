8132efd470a6d2322a82e794d35b1ca39df2e2ca
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-12 21:28:39 -0300

    feat(autenticação): implementa exibição das mensagens na view cadastrar

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 # Create your views here.
@@ -13,10 +15,12 @@ def cadastrar(request):
         senha = request.POST.get('senha')
         # validação básica
         if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos corretamente')
             return redirect('/auth/cadastrar')
         # consulta no Módulo Admin se existe usuário com o username passado
         user = User.objects.filter(username=username)
         if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário cadastrado com o username informado')
             return redirect('/auth/cadastrar')
         # cadastrar usuário no Banco de Dados
         try:
@@ -24,8 +28,11 @@ def cadastrar(request):
                                             email=email,
                                             password=senha)
             user.save()
+            messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
             return redirect('/auth/logar')
         except:
+            messages.add_message(request, constants.ERROR,
+                                 'Erro interno inesperado! Tente novamente ou contate o Administrador do Sistema')
             return redirect('/auth/cadastrar')
 
 
